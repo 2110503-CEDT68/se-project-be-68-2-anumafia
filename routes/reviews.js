1bc5b30b6784f10e3045d422e69d15df48d44cc1
Akibara <laanemmone@gmail.com>
2026-04-11 13:52:11 +0700

    Add updateReview route to reviews API

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const { createReview } = require("../controllers/review");
+const { createReview, updateReview } = require("../controllers/review");
 const { protect, authorize } = require("../middleware/auth");
 
 router.route("/")
     .post(protect, authorize("admin", "user"), createReview);
+
+router.route("/:id")
+	.put(protect, authorize("admin", "user"), updateReview);
 
 module.exports = router;
