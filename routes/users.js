2e280af4bfd2be877540abb2eb9019d3a3427c72
camp_.ksk <camp131249@gmail.com>
2026-04-14 19:08:42 +0700

    get single user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const { getUsers } = require("../controllers/users");
+const { getUsers, getUser } = require("../controllers/users");
 const { protect, authorize } = require("../middleware/auth");
 
 router.route("/")
     .get(protect, authorize("admin"), getUsers);
+router.route("/:id")
+    .get(protect, authorize("admin"), getUser);
 
 module.exports = router;
